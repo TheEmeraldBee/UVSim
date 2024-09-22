a15f563dc6a9aae1d54f3838ef62060b140c0877
BradyShimanek <144560398+BradyShimanek@users.noreply.github.com>
2024-09-22 16:11:02 -0600

    Added 2nd test case for test_load

--- test/test_load_instruction.py
+++ test/test_load_instruction.py
@@ -11,3 +11,12 @@ def test_load():
 
     assert vm.accumulator == 69
 
+
+def test_load_negative():
+    vm = VirtualMachine()
+    add_instruction = LoadInstruction()
+
+    vm.get_memory().set(0, -42)
+    add_instruction.handle(vm, 0)
+
+    assert vm.accumulator == -42
