955c0851b978d905da32b4bb751111d7e0821b11
BradyShimanek <144560398+BradyShimanek@users.noreply.github.com>
2024-09-22 18:18:47 -0600

    Added comments to explain code in repl.py

--- src/main.py
+++ src/main.py
@@ -13,24 +13,24 @@ class Main:
         """Run Program Here"""
         args = sys.argv
 
-        # Ensure we have 1 argument (1 for the python file, and 1 for the filepath to run)
+        # Ensure we have 1 argument (1 for the python file, and 1 for the filepath to run).
         if len(args) != 2:
             raise Exception(f"Expected 1 argument, found {len(args) - 1}")
 
 
-        # Open the file from the passed filepath
+        # Open the file from the passed filepath.
         path = args[1]
         with open(path, "r") as f:
             lines = f.readlines()
             for i in range(0, len(lines)):
-                # Try to set the memory with the line, while catching errors parsing extructions
+                # Try to set the memory with the line, while catching errors parsing extructions.
 
                 try:
                     self.vm.get_memory().set(i, int(lines[i]))
                 except ValueError:
                     raise Exception(f"Invalid File Code `{lines[i]}`, should be an integer.")
                 
-        # Step through the VM, handling each instruction in the program memory
+        # Step through the VM, handling each instruction in the program memory.
         while self.vm.step():
             ...
 
--- src/repl.py
+++ src/repl.py
@@ -14,17 +14,23 @@ class Main:
             match text:
                 case 'memory':
                     print("\nProgram Memory: ")
+                    # If 'memory' is entered in the program, all memory values (0 - 99) are printed in the vm.
+
                     for i in range(0, 100):
                         print(f" - {i} = {self.vm.get_memory().get(i)}")
 
                     print("-" * 10)
 
                     continue
+                # If 'accumulator' is entered in the program, the current value of the acummulator is printed.
+
                 case 'accumulator':
                     print(f"\nProgram Accumulator is currently: {self.vm.accumulator}")
                     continue
                 case 'quit':
                     return
+                
+            # Tries to see if the input is a recognized command. If so, the instruction is printed. Otherwise an error is thrown.
             try:
                 result = self.vm.handle(int(text))
                 print(f"Instruction returned: {result}")
